cc2f5dc7f0cef30d04611e91cda4cb598ee21d0e
Andrew McGuier <amcguier@pluto.home.arpa>
2023-03-24 04:22:49 -0400

    Updating the configuration object to match Ionide configuration

--- eglot-fsharp.el
+++ eglot-fsharp.el
@@ -195,11 +195,21 @@
     (unless (eglot-fsharp-current-version-p version)
       (eglot-fsharp--install-core version))))
 
+;;; create buffer local settings for workspace reload based on mode hook
+
+(defun eglot-fsharp--set-workspace-args ()
+  "Set a buffer local variable with the workspace settings for eglot."
+  (make-local-variable 'eglot-workspace-configuration)
+  (let ((settings-json (json-serialize eglot-fsharp-fsautocomplete-args)) )
+    (setq eglot-workspace-configuration settings-json ))
+  )
+
 ;;;###autoload
 (defun eglot-fsharp (interactive)
   "Return `eglot' contact when FsAutoComplete is installed.
 Ensure FsAutoComplete is installed (when called INTERACTIVE)."
   (when interactive (eglot-fsharp--maybe-install))
+  (eglot-fsharp--set-workspace-args)
   (cons 'eglot-fsautocomplete
         (if (file-remote-p default-directory)
             `("sh" ,shell-command-switch ,(concat "cat|"  (mapconcat #'shell-quote-argument
@@ -224,15 +234,6 @@ Ensure FsAutoComplete is installed (when called INTERACTIVE)."
          (not (equal (cadddr err) '(jsonrpc-error-message . "Could not find declaration")))))
     (when (cl-next-method-p)
       (cl-call-next-method))))
-
-;;; create buffer local settings for workspace reload based on mode hook
-
-(defun eglot-fsharp--set-workspace-args ()
-  "Set a buffer local variable with the workspace settings for eglot."
-  (make-local-variable 'eglot-workspace-configuration)
-  (let ((settings-json (json-serialize `(:fSharp ,eglot-fsharp-fsautocomplete-args))) )
-    (setq eglot-workspace-configuration settings-json ))
-  )
 
 (add-to-list 'eglot-server-programs `(fsharp-mode . eglot-fsharp))
 
